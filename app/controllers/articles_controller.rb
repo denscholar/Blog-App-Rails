83e2de3f6d25abf82f5eb22e679fe6577a52eddd
Dennis Akagha <d.akagha20@gmail.com>
2022-07-30 17:32:13 +0100

    Refactor my code to so as to implement DRY

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
     def show
-        @article = Article.find(params[:id])
     end
 
     def index 
@@ -12,12 +13,11 @@ class ArticlesController < ApplicationController
     end
 
     def edit
-        @article = Article.find(params[:id])
     end
 
     def create 
         # render plain: params[:article]
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
             flash[:notice] = 'Data saved successfully'
             redirect_to @article
@@ -27,13 +27,28 @@ class ArticlesController < ApplicationController
     end
 
     def update
-        @article = Article.find(params[:id])
-        if @article.update(params.require(:article).permit(:title, :description))
+        if @article.update(article_params)
             flash[:notice] = 'Data updated successfully'
             redirect_to @article
         else
             render 'edit'
         end
     end
+
+    def destroy
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+    
+    def article_params
+        params.require(:article).permit(:title, :description)
+    end
+    
     
 end

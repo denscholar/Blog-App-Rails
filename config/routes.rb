b771d11a71f5ec52d4401854b970a9cb6ad8f8ea
Dennis Akagha <d.akagha20@gmail.com>
2022-08-01 08:50:58 +0100

    Add signup feature

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  
+
   # Defines the root path route ("/")
   root "pages#home"
   get "about", to: "pages#about"
   resources :articles
+  get "signup", to: "users#new"
+  resources :users, except: [:new]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -25,5 +25,4 @@ ActiveRecord::Schema[7.0].define(version: 2022_07_31_202635) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
 end

b0095733d6e3417525c8b8c50b114bd83f88aa60
Dennis Akagha <d.akagha20@gmail.com>
2022-08-01 11:36:58 +0100

    Add user from the backend

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,22 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def create
+    @user = User.new(user_params)
+    @user.save
+    if @user.save
+      flash[:notice] = "Welcome #{@user.username}!, You have successfully logged in"
+      redirect_to articles_path
+    else
+      render "new"
+    end
+  end
+
+  private
+
+  def user_params
+      params.require(:user).permit(:username, :email, :password)
+  end
+  
 end
